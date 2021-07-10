cd740a7e0aa931471590b5dd5c2eb76b2b047ff3
Ishit-1290 <vinee042006@gmail.com>
2021-07-10 23:40:49 +0530

    updated program.java

--- program.java
+++ program.java
@@ -2,8 +2,8 @@ import java.util.*;
 class program{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
         int b = sc.nextInt();
+        int a = sc.nextInt();
         System.out.println(a + b);
         sc.close();
     }
